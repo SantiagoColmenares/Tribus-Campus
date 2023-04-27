b3ca2015631b2bff3e557f3a9870ef4d48eaf061
SantiagoColmenaresCampus <santiagocolmeasmc0423@gmail.com>
2023-04-27 17:13:21 -0500

    Se solcuiono la tarea pendiente.

--- js/campers.js
+++ js/campers.js
@@ -3,36 +3,46 @@ const btnData = document.querySelector('#btnData')
 
 btnData.addEventListener("click", getData)
 
-function getData(){
+function getData() {
+    btnData.style.display = "none"
     fetch(URL_API)
-    .then((response) =>{
-        return response.json();
-    })
-    .then((data) => {
-        viewData(data);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            viewData(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
 
-function viewData(data){
-    const divContenedor = document.querySelector('#contenedor')
+function viewData(data) {
+    const conCampers = document.querySelector('.conCampers')
+    const divContenedor = document.querySelector('.contenedor')
     data.forEach(element => {
-        const {idTribu,tribu,puntos,campers} = element;
+        const { idTribu, tribu, puntos, campers } = element;
         divContenedor.innerHTML = /* html */ `
-        <p>Id: ${idTribu}</p>
-        <p>Tribu: ${tribu}</p>
-        <p>Puntos: ${puntos}</p>
+        <div class="tribu-item">
+            <p>Id: ${idTribu}</p>
+            <p>Tribu: ${tribu}</p>
+            <p>Puntos: ${puntos}</p>
+            <p>Campers:</p>
+        </div>
     `
-
-    data.campers.forEach(camper => {
-        const {id,nombre,edad,ingles,rol,img} = camper;
-        console.log(id);
-        // Hacer algo con cada camper...
+        campers.forEach(camper => {
+            const { id, nombre, edad, ingles, rol, img } = camper;
+            conCampers.innerHTML += /* html */`
+        <div class="camper-item">
+            <li>Id Camper: ${camper.id}</li>
+            <li>Nombre: ${camper.nombre}</li>
+            <li>Edad: ${camper.edad}</li>
+            <li>Ingles: ${camper.ingles}</li>
+            <li>Rol: ${camper.rol}</li>
+            <li>Foto: ${camper.img}</li>
+        </div>
+    `
+        });
     });
-    });
-  
-    
 }
 
